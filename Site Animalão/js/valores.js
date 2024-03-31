739a9b7627c6bdc0f064645bd8bf229c228f9080
joao <joaovictor314@hotmail.com>
2024-03-31 19:22:28 -0300

    Add fundadoras

--- Site Animalão/js/valores.js
+++ Site Animalão/js/valores.js
@@ -1,4 +1,4 @@
-//Para a adoação
+//Para valores
 
 const myObserver = new IntersectionObserver((natela) => {
     natela.forEach((natela) => {
@@ -12,7 +12,25 @@ const myObserver = new IntersectionObserver((natela) => {
 
 })
 
- // Para a adoação
+const myObserver1 = new IntersectionObserver((natela) => {
+    natela.forEach((natela) => {
+        if(natela.isIntersecting){
+            natela.target.classList.add('visivel-time')
+        } else {
+            natela.target.classList.remove('visivel-time')
+        }
+
+    })
+
+})
+
+ // Para a valores
  const elementos = document.querySelectorAll('.invisivel-valores')
 
  elementos.forEach((elemento) => myObserver.observe(elemento))
+
+ // Para time
+
+const elementos1 = document.querySelectorAll('.invisivel-time')
+
+elementos1.forEach((elemento1) => myObserver1.observe(elemento1))
